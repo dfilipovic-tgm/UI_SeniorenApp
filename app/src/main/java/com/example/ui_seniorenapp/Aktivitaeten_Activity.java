801b3989d85a7929ee7913008d126e66db016821
Daniel Filipovic <31840771+dfilipovic-tgm@users.noreply.github.com>
2020-02-25 13:47:48 +0100

    GUI

---
+++ app/src/main/java/com/example/ui_seniorenapp/Aktivitaeten_Activity.java
@@ -0,0 +1,14 @@
+package com.example.ui_seniorenapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Aktivitaeten_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aktivitaeten_);
+    }
+}
