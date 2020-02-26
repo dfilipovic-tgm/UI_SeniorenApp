f07a90c4d51567d1566468640eb0b4681239a0ea
fgavric-tgm <fgavric@student.tgm.ac.at>
2020-02-26 13:11:01 +0100

    Google Maps

--- app/src/main/java/com/example/ui_seniorenapp/MainActivity.java
+++ app/src/main/java/com/example/ui_seniorenapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.ui_seniorenapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,9 +13,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profil);
+        setContentView(R.layout.activity_homescreen);
 
-        /**Button button= (Button) findViewById(R.id.button3);
+        Button button= (Button) findViewById(R.id.button3);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
@@ -31,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
 
             }
-        });**/
+        });
 
     }
 }
