5405e3dd3dd2ff41dbc7385266dcc47dad889285
fgavric-tgm <fgavric@student.tgm.ac.at>
2020-02-26 12:37:02 +0100

    Google Maps

---
+++ app/src/main/java/com/example/ui_seniorenapp/klub_info.java
@@ -0,0 +1,14 @@
+package com.example.ui_seniorenapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class klub_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_klub_info);
+    }
+}
