eeed74920f558f22e27a03738a7fa7df1daefdc9
Daniel Filipovic <31840771+dfilipovic-tgm@users.noreply.github.com>
2020-02-26 11:09:40 +0100

    Chat fertig

--- app/src/main/java/com/example/ui_seniorenapp/MainActivity.java
+++ app/src/main/java/com/example/ui_seniorenapp/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.ui_seniorenapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_aktivitaet);
+        setContentView(R.layout.activity_homescreen);
+
+        Button button = (Button) findViewById(R.id.button8);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, ChatActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
