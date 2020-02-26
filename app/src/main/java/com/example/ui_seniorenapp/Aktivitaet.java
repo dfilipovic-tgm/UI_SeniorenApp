9c15c379986a72eeb14b672770eaf01fb001dbd4
Daniel Filipovic <31840771+dfilipovic-tgm@users.noreply.github.com>
2020-02-26 10:00:56 +0100

    Akktivität fertig

--- app/src/main/java/com/example/ui_seniorenapp/Aktivitaet.java
+++ app/src/main/java/com/example/ui_seniorenapp/Aktivitaet.java
@@ -2,7 +2,12 @@ package com.example.ui_seniorenapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Aktivitaet extends AppCompatActivity {
 
