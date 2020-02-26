27a0e4fbb1fde16834ee2173b66f75579eb8cc37
Daniel Filipovic <31840771+dfilipovic-tgm@users.noreply.github.com>
2020-02-26 12:00:51 +0100

    App ist meier

--- app/src/main/java/com/example/ui_seniorenapp/MainActivity.java
+++ app/src/main/java/com/example/ui_seniorenapp/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_homescreen);
+        setContentView(R.layout.activity_profil);
 
         Button button= (Button) findViewById(R.id.button3);
         button.setOnClickListener(new View.OnClickListener() {
