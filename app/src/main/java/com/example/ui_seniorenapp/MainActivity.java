b0091a174bfca71a1c6ea40d1e3d6fb574f9d52d
Daniel Filipovic <31840771+dfilipovic-tgm@users.noreply.github.com>
2020-02-26 11:13:36 +0100

    MainActivity

--- app/src/main/java/com/example/ui_seniorenapp/MainActivity.java
+++ app/src/main/java/com/example/ui_seniorenapp/MainActivity.java
@@ -1,20 +1,19 @@
 package com.example.ui_seniorenapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_aktivitaet);
+        setContentView(R.layout.activity_homescreen);
 
         Button button= (Button) findViewById(R.id.button3);
         button.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +23,16 @@ public class MainActivity extends AppCompatActivity {
                 Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
                 mapIntent.setPackage("com.google.android.apps.maps");
                 startActivity(mapIntent);
+
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.button8);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, ChatActivity.class);
+                startActivity(intent);
 
             }
         });
