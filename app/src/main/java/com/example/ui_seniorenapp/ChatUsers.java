4be2857fdefc28a4d1a9a04513c65090a238a75c
Daniel Filipovic <31840771+dfilipovic-tgm@users.noreply.github.com>
2020-03-26 14:16:49 +0100

    Bugfixing

--- app/src/main/java/com/example/ui_seniorenapp/ChatUsers.java
+++ app/src/main/java/com/example/ui_seniorenapp/ChatUsers.java
@@ -1,6 +1,8 @@
 package com.example.ui_seniorenapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +14,8 @@ public class ChatUsers extends AppCompatActivity {
         setContentView(R.layout.activity_chat_users);
     }
 
-
+    public void goToChat(View view) {
+        startActivity(new Intent(ChatUsers.this, ChatActivity.class));
+    }
 
 }
